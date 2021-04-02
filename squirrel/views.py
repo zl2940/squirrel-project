d99674cedbd55ae66b2fc6178a5848c6ab2cd70f
Minjie Wang <mw3452@columbia.edu>
2021-04-02 07:41:07 +0000

    Template

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
+def index(request):
+    return render (request, 'squirrel/index.html',{})
 # Create your views here.
