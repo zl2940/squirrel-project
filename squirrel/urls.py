600a2661b47127288a18e37e9fdd0f0d26ce09ef
zl2940 <zl2940@columbia.edu>
2021-04-10 11:03:30 +0000

    urls.py

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index),
- ]
+        path('',views.sightings, name='sightings'),
+        path('create/',views.create, name='create'),
+        path('stats/',views.stats, name='stats'),
+        path('<str:Unique_Squirrel_ID>/',views.update,name='update'), 
+]
+
