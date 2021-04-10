639a3221ce8754c008552c992c402fc8af648522
zl2940 <zl2940@columbia.edu>
2021-04-10 10:11:51 +0000

    views.py

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-
+from django.db.models import Avg, Max, Min, Count, Q
 from .models import Squirrel
 
 def sightings(request):
@@ -33,16 +33,32 @@ def update_squirrel(request,Unique_Squirrel_ID):
             return redirect('/squirrel/')
     else:
         form = SquirrelForm(instance=squirrel)
-    context ={
+        context ={
             'form':form,
              }
     return render(request, 'squirrel/update.html', context)
 
 def stats(request):
-
-
-
-
+    squirrels = Squirrel.objects.all()
+    Total=len(squirrels)
+    Adult_count=Squirrel.objects.filter(Age="Adult").count(),
+    Juvenile_count=Squirrel.objects.filter(Age="Juvenile").count(),
+    Gray_count=Squirrel.objects.filter(Primary_Fur_Color="Gray").count()
+    Cinnamon_count=Squirrel.objects.filter(Primary_Fur_Color="Cinnamon").count(),
+    Black_count=Squirrel.objects.filter(Primary_Fur_Color="Black").count(),
+    Ground_count=Squirrel.objects.filter(Location="Ground Plane").count(),
+    Above_count=Squirrel.objects.filter(Location="Above Ground").count(),
+    context = {
+            'Total_Sightings': Total,
+            'Adult_Count': Adult_Count,
+            'Juvenile_count': Juvenile_count,
+            'Gray_count': Gray_count,
+            'Cinnamon_count': Cinnamon_count,
+            'Black_count': Black_count,
+            'Ground_count': Ground_count,
+            'Above_count': Above_count,
+            }
+    return render(request, 'squirrel/stats.html', context)
 
 
 
