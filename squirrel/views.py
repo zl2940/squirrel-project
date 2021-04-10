de88df9f4815d969ad2849104a6494f2f51430d4
zl2940 <zl2940@columbia.edu>
2021-04-10 08:25:24 +0000

    edit views.py

--- squirrel/views.py
+++ squirrel/views.py
@@ -11,6 +11,39 @@ def index(request):
             }
     return render(request,'squirrel/index.html',context)
 
+def create_squirrel(request):
+    if request.method=='POST':
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/squirrel/')
+    else:
+        form = SquirrelForm(request.POST)
+        contect={
+                'form'=form,
+                }
+    return render(request,'sightings/create.html',context)
+
+def update_squirrel(request,Unique_Squirrel_ID):
+    squirrel= Squirrel.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
+    if request.method =='POST':
+        form = SquirrelForm(request.POST, instance=squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect('/squirrel/')
+    else:
+        form = SquirrelForm(instance=squirrel)
+    context ={
+            'form':form,
+             }
+    return render(request, 'sightings/update.html', context)
+
+
+
+
+
+
+
 
 
 # Create your views here.
