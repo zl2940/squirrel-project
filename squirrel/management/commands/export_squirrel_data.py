6b14746346f35c30c8cde4abdc7f007d6c6be15c
zl2940 <zl2940@columbia.edu>
2021-04-11 05:00:55 +0000

    export

--- squirrel/management/commands/export_squirrel_data.py
+++ squirrel/management/commands/export_squirrel_data.py
@@ -3,19 +3,17 @@ import csv
 from squirrels.models import Squirrel
 
 class Command(BaseCommand):
-	help='Export Squirrel Data'
-
-	def add_arguments(self, parser):
-		parser.add_argument('path',type=str)
-	
-	def handle(self, *args,**kwargs):
-		path = kwargs['path']
-		from django.apps import apps
-                Squirrel_M = apps.get_model('squirrel', 'Squirrel')
-		name = [n.name for n in Squirrel_M._meta.fields]
-		with open(path, 'w') as csvfile:
-			writer = csv.writer(csvfile)
-			writer.writerow(name)
-			for squirrel in Squirrel_M.objects.all():
-				writer.writerow([getattr(squirrel,n) for n in name])
+    help='Export Squirrel Data'
+    def add_arguments(self, parser):
+        parser.add_argument('path',type=str)
+    def handle(self, *args,**kwargs):
+        path = kwargs['path']
+        from django.apps import apps
+        Squirrel_M = apps.get_model('squirrel', 'Squirrel')
+        name = [n.name for n in Squirrel_M._meta.fields]
+        with open(path, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(name)
+            for squirrel in Squirrel_M.objects.all():
+                writer.writerow([getattr(squirrel,n) for n in name])
 
